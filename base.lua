4bd472bc31df5e5669c291db85c0db8ea3bca642
bid3l <bidel1259@gmail.com>
2017-04-10 23:28:38 -0700

    Update base.lua

--- base.lua
+++ base.lua
@@ -14,17 +14,3 @@ end
 function dl_cb (arg, data)
 end
 
-reload()
-
-function tdcli_update_callback(data)
-  update(data, tabchi_id)
-  if data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "/reload" and data.message_.sender_user_id_ == tonumber(redis:get("tabchi:" .. tabchi_id ..":fullsudo")) then
-    reload()
-    tdcli.sendMessage(data.message_.chat_id_, 0, 1, "*Bot Reloaded*", 1, "md")
-  elseif data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "/gitpull" and data.message_.sender_user_id_ == tonumber(redis:get("tabchi:" .. tabchi_id ..":fullsudo")) then
-    io.popen("git fetch --all && git reset --hard origin/master && git pull origin master"):read("*all")
-     io.popen("chmod 777 telegamer.sh"):read("*all")
-    reload()
-    tdcli.sendMessage(data.message_.chat_id_, 0, 1, "*Updates Received And Bot Reloaded*", 1, "md")
-  end
-end
